70ef564a9a9b61da5c1bf50e03847065712255e5
Andrew <Andrew@DESKTOP-84M90NM>
2020-04-09 23:19:37 +0300

    init

--- src/main/java/com/example/demo/controller/StockController.java
+++ src/main/java/com/example/demo/controller/StockController.java
@@ -1,10 +1,12 @@
-package com.example.demo;
+package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.TaskScheduler;
 import org.springframework.stereotype.Controller;
+
+import com.example.demo.model.Stock;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
