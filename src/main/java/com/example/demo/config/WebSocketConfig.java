bed031c4f324887a244472848b2c547d8fe84d98
Andrew <Andrew@DESKTOP-VUUTVQ9>
2020-04-11 12:24:47 +0300

    init

--- src/main/java/com/example/demo/config/WebSocketConfig.java
+++ src/main/java/com/example/demo/config/WebSocketConfig.java
@@ -1,27 +1,27 @@
 package com.example.demo.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import com.example.demo.EchoHandler;
 
 /**
  * Created by iuliana.cosmina on 7/29/17.
  */
 @Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+@EnableWebSocket
+public class WebSocketConfig implements WebSocketConfigurer {
 
 	@Override
-	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		registry.addEndpoint("/ws").withSockJS();
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(echoHandler(), "/echoHandler").withSockJS();
 	}
 
-	@Override
-	public void configureMessageBroker(MessageBrokerRegistry config) {
-		config.setApplicationDestinationPrefixes("/app");
-		config.enableSimpleBroker("/topic");
+	@Bean
+	public EchoHandler echoHandler() {
+		return new EchoHandler();
 	}
-
 }
